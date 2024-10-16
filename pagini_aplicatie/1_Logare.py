fd3954a13941020a7813455ce2c3b68735468bdf
florinhegedus <florinxhgds@gmail.com>
2024-10-16 15:28:59 +0300

    removed columns from login page

--- pagini_aplicatie/1_Logare.py
+++ pagini_aplicatie/1_Logare.py
@@ -7,11 +7,11 @@ st.title("Logare")
 col1,col2,col3 = st.columns([1,2,1])
 
 # Authentication form layout
-do_you_have_an_account = col2.selectbox(label='Aveți cont?',options=('Da','Nu','Mi-am uitat parola'))
-auth_form = col2.form(key='Authentication form',clear_on_submit=False)
+do_you_have_an_account = st.selectbox(label='Aveți cont?', options=('Da', 'Nu', 'Mi-am uitat parola'))
+auth_form = st.form(key='Authentication form', clear_on_submit=False)
 email = auth_form.text_input(label='Email')
-password = auth_form.text_input(label='Password',type='password') if do_you_have_an_account in {'Da','Nu'} else auth_form.empty()
-auth_notification = col2.empty()
+password = auth_form.text_input(label='Password', type='password') if do_you_have_an_account in {'Da', 'Nu'} else auth_form.empty()
+auth_notification = st.empty()
 
 # Sign In
 if do_you_have_an_account == 'Da' and auth_form.form_submit_button(label='Logare',use_container_width=True,type='primary'):
